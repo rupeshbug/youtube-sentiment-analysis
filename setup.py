35a4804a43d23bdc1e062d3cbbeaa10f2d907d03
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-03-20 11:35:35 +0545

    logger and custom exception file

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='student_performance',
+name='youtube-sentiment-analysis',
 version='0.0.1',
 author='Rupesh',
 author_email='chaulagainrupesh1@gmail.com',
